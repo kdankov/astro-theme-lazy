e17d8c4c248491d8512cebbd7e905c658a663ba0
Konstantin Dankov <konstantin.dankov@gmail.com>
2024-12-14 12:06:48 +0200

    Layout setup, blog and contasts

---
+++ src/utils/constants.ts
@@ -0,0 +1,14 @@
+export const site = {
+    title: 'Lazy',
+    description: 'Easy'
+};
+
+
+export const links = {
+    LIKE_THIS: 'https://some.social.media.lins.com/my-account',
+};
+
+export const ui = {
+    MAX_ITEMS: 100,
+    DEFAULT_LANGUAGE: 'en',
+};
